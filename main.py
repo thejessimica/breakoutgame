5a11195d50e32f6e8085f963328260b234177e37
Jessica <94.jessicaschmidt@gmail.com>
2024-01-24 16:22:36 -0700

    Add Block class, initialize blocks

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 from turtle import Screen
+from blocks import Block
+import time
 
 
 screen = Screen()
@@ -7,5 +9,20 @@ screen.bgcolor("black")
 screen.title("Breakout")
 screen.tracer(0)
 
+
+number_of_squares = 10
+spacer = -400
+
+
+for _ in range(number_of_squares):
+    time.sleep(1)
+    screen.update()
+    block = Block((spacer, 280))
+    spacer += 120
+    screen.update()
+
+
+
+screen.listen()
 
 screen.exitonclick()
