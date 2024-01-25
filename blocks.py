a25fc6b82749a4740f2eaec62fcfe7ddfe0c5abd
Jessica <94.jessicaschmidt@gmail.com>
2024-01-24 17:16:00 -0700

    Adjust block layout, add click recognition

--- blocks.py
+++ blocks.py
@@ -3,18 +3,15 @@ from turtle import Turtle
 
 class Block(Turtle):
 
-    def __init__(self, position):
+    def __init__(self, position, color, name):
         super().__init__()
         self.shape("square")
-        self.color("red")
+        self.color(color)
         self.penup()
         self.shapesize(stretch_wid=1, stretch_len=5)
         self.goto(position)
+        self.name = name
 
-    # def go_up(self):
-    #     new_y = self.ycor() + 20
-    #     self.goto(self.xcor(), new_y)
-    #
-    # def go_down(self):
-    #     new_y = self.ycor() - 20
-    #     self.goto(self.xcor(), new_y)
+    def delete_block(self, x, y):
+        self.clear()
+        self.hideturtle()
--- main.py
+++ main.py
@@ -11,18 +11,50 @@ screen.tracer(0)
 
 
 number_of_squares = 10
-spacer = -400
 
 
-for _ in range(number_of_squares):
-    time.sleep(1)
+def create_row(num, height, color, offset):
+    spacer = -600 + offset
+    all_blocks = []
+    for _ in range(num):
+        # time.sleep(0.1)
+        screen.update()
+        block_name = f"Block_{color}_{_}"
+        block = Block((spacer, height), color, block_name)
+        all_blocks.append(block)
+        spacer += 120
+        screen.update()
+        print(block_name)
+
+
+def refresh_blocks():
+    create_row(10, 280, "red", 0)
+    create_row(10, 250, "orange", 10)
+    create_row(10, 220, "yellow", 40)
+    create_row(10, 190, "green", 80)
+    create_row(10, 160, "blue", 120)
+    create_row(10, 130, "purple", 160)
+
+
+def get_mouse_click_coor(x, y):
+    x_coord = x
+    y_coord = y
+    print(x_coord, y_coord)
     screen.update()
-    block = Block((spacer, 280))
-    spacer += 120
-    screen.update()
 
+
+screen.onscreenclick(get_mouse_click_coor)
+
+
+refresh_blocks()
+
+screen.update()
 
 
 screen.listen()
 
-screen.exitonclick()
+
+# screen.onclick(delete_block)
+
+screen.mainloop()
+# screen.exitonclick()
